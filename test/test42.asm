f21c9a03da9127618a53cfee1c17abd8ad83fd29
rk0der <ryo.alter@gmail.com>
2015-11-08 23:48:30 +0900

    create test for bios

---
+++ test/test42.asm
@@ -0,0 +1,21 @@
+BITS 32
+  org 0x7c00
+start:
+  mov esi, msg
+  call puts
+  jmp 0
+
+puts:
+  mov al, [esi]
+  inc esi
+  cmp al, 0
+  je puts_end
+  mov ah, 0x0e
+  mov ebx, 15
+  int 0x10
+  jmp puts
+puts_end:
+  ret
+
+msg:
+  db "hello, world", 0x0d, 0x0a, 0
